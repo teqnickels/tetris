2ad3b0173f289e3e9e839d2b62460de9cfde491d
TeqNickels <debrenamcewen@gmail.com>
2018-04-20 23:14:56 -0700

    troubleshooting heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'tetris.html'));
+  res.sendFile('tetris.html');
 });
 
 app.listen(port);
