ee89ea12ee2491ca7437da9a13557b97a6fe9515
TeqNickels <debrenamcewen@gmail.com>
2018-07-27 09:58:09 -0700

    fix missing preview window

--- public/js/tetris.js
+++ public/js/tetris.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 	var canvas = document.getElementsByClassName('board')[0];
 	var ctx = canvas.getContext("2d");
 	var linecount = document.getElementsByClassName('lines')[0];
@@ -14,7 +14,7 @@ $(function() {
 	vid.playbackRate = 2.0;
 
 	canvas.width = width * tilesz;
-	canvas.height = height * tilesz; 
+	canvas.height = height * tilesz;
 
 	var board = [];
 	for (var r = 0; r < height; r++) {
@@ -60,7 +60,7 @@ $(function() {
 		drawOnDeck();
 		return piece;
 	}
-	
+
 	//Clearing the piece on deck before adding the next piece on deck
 	function clearDeck() {
 		var onDeckCanvas = document.getElementsByClassName("tetron-next-piece")[0]
@@ -81,15 +81,15 @@ $(function() {
 		this.patterns = patterns;
 		this.patterni = 0;
 		this.color = color;
-		this.x = width/2-parseInt(Math.ceil(this.pattern.length/2), 10);
+		this.x = width / 2 - parseInt(Math.ceil(this.pattern.length / 2), 10);
 		this.y = -2;
 	}
 
-	Piece.prototype.rotate = function() {
+	Piece.prototype.rotate = function () {
 		var nudge = 0;
 		var nextpat = this.patterns[(this.patterni + 1) % this.patterns.length];
 
-		if(this._collides(0, 0, nextpat)) {
+		if (this._collides(0, 0, nextpat)) {
 			nudge = this.x > width / 2 ? -1 : 1;
 		}
 
@@ -105,7 +105,7 @@ $(function() {
 	var WALL = 1;
 	var BLOCK = 2;
 
-	Piece.prototype._collides = function(dx, dy, pat) {
+	Piece.prototype._collides = function (dx, dy, pat) {
 		for (var ix = 0; ix < pat.length; ix++) {
 			for (var iy = 0; iy < pat.length; iy++) {
 				if (!pat[ix][iy]) {
@@ -127,7 +127,7 @@ $(function() {
 		return 0;
 	};
 
-	Piece.prototype.down = function() {
+	Piece.prototype.down = function () {
 		if (this._collides(0, 1, this.pattern)) {
 			this.lock();
 			piece = newPiece();
@@ -138,7 +138,7 @@ $(function() {
 		}
 	};
 
-	Piece.prototype.moveRight = function() {
+	Piece.prototype.moveRight = function () {
 		if (!this._collides(1, 0, this.pattern)) {
 			this.undraw(ctx);
 			this.x++;
@@ -146,7 +146,7 @@ $(function() {
 		}
 	};
 
-	Piece.prototype.moveLeft = function() {
+	Piece.prototype.moveLeft = function () {
 		if (!this._collides(-1, 0, this.pattern)) {
 			this.undraw(ctx);
 			this.x--;
@@ -157,7 +157,7 @@ $(function() {
 	var lines = 0;
 	var done = false;
 
-	Piece.prototype.lock = function() {
+	Piece.prototype.lock = function () {
 		for (var ix = 0; ix < this.pattern.length; ix++) {
 			for (var iy = 0; iy < this.pattern.length; iy++) {
 				if (!this.pattern[ix][iy]) {
@@ -182,7 +182,7 @@ $(function() {
 			if (line) {
 				for (var y2 = y; y2 > 1; y2--) {
 					for (var x = 0; x < width; x++) {
-						board[y2][x] = board[y2-1][x];
+						board[y2][x] = board[y2 - 1][x];
 					}
 				}
 				for (var x = 0; x < width; x++) {
@@ -192,7 +192,7 @@ $(function() {
 				console.log("LINES", nlines)
 			}
 		}
-		
+
 		let previousLines = linePoints.textContent
 		let currentLines;
 		var countUntilLevelUp = 5;
@@ -202,11 +202,11 @@ $(function() {
 			level.textContent = "Level: " + getLevel(lines)
 			drawBoard();
 			linecount.textContent = "Lines: " + lines;
-		 }
+		}
 
 	};
 
-	Piece.prototype._fill = function(ctx, color, yOffset = 0 ) {
+	Piece.prototype._fill = function (ctx, color, yOffset = 0) {
 		var fs = ctx.fillStyle;
 		ctx.fillStyle = color;
 		var x = this.x;
@@ -222,11 +222,11 @@ $(function() {
 		ctx.fillStyle = fs;
 	};
 
-	Piece.prototype.undraw = function(ctx) {
+	Piece.prototype.undraw = function (ctx) {
 		this._fill(ctx, clear);
 	};
 
-	Piece.prototype.draw = function(ctx, yOffset) {
+	Piece.prototype.draw = function (ctx, yOffset) {
 		this._fill(ctx, this.color, yOffset);
 	};
 
@@ -234,23 +234,23 @@ $(function() {
 	var dropStart = Date.now();
 	var downI = {};
 
-	$('body').keydown(function(event) {
-	    if(event.which == 38){
-	      piece.rotate()
-	      dropStart = Date.now()
-	    }
-	    if(event.which == 40){
-	      piece.down()
-	    }
-	    if(event.which == 37){
-	      piece.moveLeft()
-	      dropStart = Date.now()
-	    }
-	    if(event.which == 39){
-	      piece.moveRight()
-	      dropStart = Date.now()
-	    }
-		})
+	$('body').keydown(function (event) {
+		if (event.which == 38) {
+			piece.rotate()
+			dropStart = Date.now()
+		}
+		if (event.which == 40) {
+			piece.down()
+		}
+		if (event.which == 37) {
+			piece.moveLeft()
+			dropStart = Date.now()
+		}
+		if (event.which == 39) {
+			piece.moveRight()
+			dropStart = Date.now()
+		}
+	})
 
 	function drawBoard() {
 		var fs = ctx.fillStyle;
@@ -264,51 +264,51 @@ $(function() {
 	}
 
 	function getLevel(numLines) {
-	 return	parseInt(numLines/2)
+		return parseInt(numLines / 2)
 	}
 
 
 	window.addEventListener('keydown', function (e) {
 		var key = e.keyCode;
 		if (key === 80) {
-    	togglePause();
+			togglePause();
 		}
 	});
 
-	let paused; 
+	let paused;
 
 	function togglePause() {
-			if(!paused) {
-					paused = true;
-					displayPausedModal()
-			} else if(paused) {
-					paused = false;
-					closePauseModal()
-					requestAnimationFrame(main);
-			}
+		if (!paused) {
+			paused = true;
+			displayPausedModal()
+		} else if (paused) {
+			paused = false;
+			closePauseModal()
+			requestAnimationFrame(main);
+		}
 	}
 
 	function main() {
-		var shortenTime = 0; 
+		var shortenTime = 0;
 		var now = Date.now();
 		var delta = now - dropStart;
 		var playerLevel = getLevel(lines)
-		if(playerLevel > 0) {
-			shortenTime = 50 * playerLevel; 
-			if(delta > (1000 - shortenTime)) {
+		if (playerLevel > 0) {
+			shortenTime = 50 * playerLevel;
+			if (delta > (1000 - shortenTime)) {
 				piece.down();
 				dropStart = now;
 			}
-		} else if (delta > 1000) { 
+		} else if (delta > 1000) {
 			piece.down();
 			dropStart = now;
 		}
-		if(!done && !paused) {
+		if (!done && !paused) {
 			var myRequest = requestAnimationFrame(main);
 		}
-		if(paused) {
+		if (paused) {
 			cancelAnimationFrame(myRequest);
-		} 
+		}
 	}
 
 	piece = newPiece();
@@ -316,36 +316,36 @@ $(function() {
 	linecount.textContent = "Lines: 0";
 	main();
 
-	$('.start').click(function() {
+	$('.start').click(function () {
 		location.reload();
 	});
 
-	
+
 	const pauseButton = document.getElementsByClassName('pause')[0]
 	pauseButton.addEventListener('click', togglePause)
 
 
 	// Get the modal
 	var modal = document.getElementById('myModal');
-	
-	
+
+
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
-		
-		function displayPausedModal() {
-			modal.style.display = "block";
-		}
 
-		var resumeGame = document.getElementsByClassName("resume")[0]
-		resumeGame.addEventListener('click', resumeAndUnpause)
+	function displayPausedModal() {
+		modal.style.display = "block";
+	}
 
-		function resumeAndUnpause() {
-			console.log("resuming game")
-			togglePause()
-		}
+	var resumeGame = document.getElementsByClassName("resume")[0]
+	resumeGame.addEventListener('click', resumeAndUnpause)
+
+	function resumeAndUnpause() {
+		console.log("resuming game")
+		togglePause()
+	}
 	// When the user clicks on <span> (x), close the modal
 	span.addEventListener('click', resumeAndUnpause)
-	
+
 
 	// WHEN USE CLOSES MODAL BY CLICKING 'X', UNPAUSE
 	function closePauseModal() {
